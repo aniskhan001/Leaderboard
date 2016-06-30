9161fe268222565ae289256bddcc105d93913367
Anisuzzaman Khan <aniskhan001@outlook.com>
2016-07-01 05:08:48 +0600

    template updated

---
+++ leaderboard.js
@@ -0,0 +1,20 @@
+// console.log('What do I have next?');
+PlayerList = new Mongo.Collection('players');
+// PlayersList.insert({ name: "Naima", score: 100 });
+
+// import { Template } from 'meteor/templating';
+
+// Conditionals - Server VS CLient
+if (Meteor.isClient) {
+	// console.log('Hello Client');
+	Template.leaderboard.helpers ({
+		'player' : function () {
+			return PlayerList.find();
+		}
+	});
+}
+
+if (Meteor.isServer) {
+	// console.log('Hello Server');
+}
+
